c7af96b049a7bf8a935a601f9112c5f612ae22bd
Alfred Simkin <alfred.simkin@gmail.com>
2023-06-06 13:15:21 +0000

    improved bwa counts

--- snakemake_pipelines/count_bwa_reads/scripts/summarize_valid_reads.py
+++ snakemake_pipelines/count_bwa_reads/scripts/summarize_valid_reads.py
@@ -34,6 +34,7 @@ def calculate_score(CIGAR):
 			digits=''
 		elif char=='S' or char=='H':
 			clip+=int(digits)
+			digits=''
 		else:
 			digits=''
 	return score, size, clip
@@ -41,6 +42,8 @@ def calculate_score(CIGAR):
 coord_dict=make_coord_dict(coords_file)
 summary_dict={}
 all_reads=set([])
+#region_of_interest='v1.7_results/count_bwa_reads_outputs/primer_overlapping_reads/dhps-581_3D7-DBS1-10K-Rep11.sam'
+#all_regions=[region_of_interest]
 for region in all_regions:
 	print('region is', region)
 	primer, sample=region.split('/')[-1].split('_')
@@ -54,35 +57,47 @@ for region in all_regions:
 		read, flag, chrom, pos, CIGAR=line[0], int(line[1]), line[2], int(line[3]), line[5]
 		score, size, clip=calculate_score(CIGAR)
 		read_start, read_end=pos, pos+abs(size)
-#		if read=='FS10001583:22:BSB09425-1416:1:1102:13980:1970':
+#		if read=='FS10001583:20:BPL20303-3425:1:1116:2410:1570':
 #			print(line)
 #			print('target was', target_chrom, target_start, target_end)
 #			print('pos was', pos, 'size was', size, 'end pos was', pos+abs(size))
 #		if chrom==target_chrom and pos>=target_start and (pos+abs(size))<=target_end:
-		if score>score_threshold and flag<256 and chrom==target_chrom and (abs(read_start-target_start)<10 or abs(read_end-target_end)<10):
-			read_dict[read]=read_dict.setdefault(read, 0)+1
-#		if score>score_threshold and clip<10 and flag<256 and chrom==target_chrom and (abs(read_start-target_start)<10 or abs(read_end-target_end)<10):
-#			pass
-#		elif score>score_threshold:
-#			print('\n', line)
-#			print('target was', target_chrom, target_start, target_end)
-#			print('pos was', pos, 'size was', size, 'end pos was', pos+abs(size))
+		if score>score_threshold and flag<256 and chrom==target_chrom:
+			read_dict.setdefault(read, [0,False,False])
+			read_dict[read][0]+=1
+			if abs(read_start-target_start)<10 and not abs(read_end-target_end)<10:
+				read_dict[read][1]=True
+			elif abs(read_end-target_end)<10 and not abs(read_start-target_start)<10:
+				read_dict[read][2]=True
+			elif abs(read_start-target_start)<10 and abs(read_end-target_end)<10 and not read_dict[read][1]:
+				read_dict[read][1]=True
+			elif abs(read_start-target_start)<10 and abs(read_end-target_end)<10 and read_dict[read][1]:
+				read_dict[read][2]=True
+#			if read=='FS10001583:20:BPL20303-3425:1:1116:2410:1570' and read_dict[read][0]==2 and read_dict[read][1] and read_dict[read][2]:
+#				print('******************', target_chrom, target_start, target_end, '********************')
 	for read in read_dict:
-		if read_dict[read]==2:
+		if read_dict[read][0]==2 and read_dict[read][1] and read_dict[read][2]:
 			summary_dict[sample][primer].add(read)
 			if read in all_reads:
 				print(read, flag, pos, CIGAR, size, score)
 			all_reads.add(read)
+#	if region==region_of_interest:
+#		interest_read_dict=read_dict
 
-#for line in open(region):
-#	split_line=line.strip().split()
-#	if split_line[0] in all_reads:
-#		valid_sam.write(line)
-
-
-for sample in summary_dict:
-	print(sample)
-	print(summary_dict[sample].keys())
+#target_chrom, target_start, target_end=coord_dict['dhps-581']
+#for line in open(region_of_interest):
+#	line=line.strip().split()
+#	read, flag, chrom, pos, CIGAR=line[0], int(line[1]), line[2], int(line[3]), line[5]
+#	score, size, clip=calculate_score(CIGAR)
+#	read_start, read_end=pos, pos+abs(size)
+#	if read=='FS10001583:20:BPL20303-3425:1:1101:1190:3830':
+#		print(CIGAR, score, size, clip)
+#		if interest_read_dict[read][0]==2 and interest_read_dict[read][1] and interest_read_dict[read][2]:
+#			pass
+#		elif score>score_threshold and flag<256 and chrom==target_chrom and (abs(read_start-target_start)<10 or abs(read_end-target_end)<10):
+#			print('\n', line)
+#			print('target was', target_chrom, target_start, target_end)
+#			print('read_start:', pos, 'read_end:', pos+abs(size))
 
 all_samples=list(summary_dict.keys())
 all_primers=list(summary_dict[all_samples[0]].keys())
